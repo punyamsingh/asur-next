3c59dacdc9b70f3c3ab9dffc49e3405f45314dd9
punyamsingh <89277920+punyamsingh@users.noreply.github.com>
2023-10-19 09:45:22 +0530

    basic choice.js

---
+++ src/components/Choice.js
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import { useUser } from '../contexts/UserContext';
+
+export default function Home() {
+    const { userType } = useUser();
+
+    let redirect = null;
+
+    if (userType === undefined) {
+        redirect = (<p>
+            {/* <Choice /> */}
+        </p>);
+    } else if (userType === 'student') {
+        redirect = (<p>
+            {/* Type padhle */}
+        </p>);
+    } else if (userType === 'teacher') {
+        redirect = (<p>
+            {/* Teachinggggg */}
+        </p>);
+    }
+
+    return (
+        <div>
+            <Head>
+                <title>A.S.U.R.</title>
+                <meta name="description" content="ASUR" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            {redirect}
+        </div>
+    );
+}
