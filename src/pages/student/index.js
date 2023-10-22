d7be9bfa7ecb3e76ef65416ca18abc21a4f78cb7
Mayank Gupta <mayankgupta20231@gmail.com>
2023-10-22 20:51:56 +0530

    commit

--- src/pages/student/index.js
+++ src/pages/student/index.js
@@ -4,8 +4,8 @@ import React from 'react';
 const Student = () => {
     return (
         <div>
-            <h1>Student</h1>
-            <Login />
+            <div>Student</div>
+            {/* <Login /> */}
         </div>
     );
 };
