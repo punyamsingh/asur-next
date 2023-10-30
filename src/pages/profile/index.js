c7ae26840455166b2bbe9b2452fbd112e8127ad4
Mayank Gupta <mayankgupta20231@gmail.com>
2023-10-30 14:46:07 +0530

    student profile update

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 // import { useUser } from '../../contexts/UserContext';
 import {useUser} from '@/contexts/UserContext';
 import Choice from '@/components/Choice';
+import Navbar from '@/components/Navbar';
+import profile from '@/styles/Profile.module.css'
 
 const Profile = () => {
     const { userType } = useUser();
@@ -12,8 +14,30 @@ const Profile = () => {
         <div>
           {console.log(userType)}
           {userType === 'student' ? <>
-            {/* Student profile */}
-            <h1>Student</h1>
+            {
+               
+            <div>
+              
+              <div className={profile.navigation_bar}>
+                <Navbar/>
+              </div>
+
+              <div className={profile.profile_container}>
+                   
+                   <div className={profile.profile}>
+                        
+                   </div>
+              </div>
+
+
+
+            
+
+            </div>
+              
+              
+              }
+            
           </> : <>
             {/* Teacher profile */}
             <h1>Teacher</h1>
