c9a9a121b8412ed4238134445eee5eb008a04299
Mayank Gupta <mayankgupta20231@gmail.com>
2023-10-30 14:46:31 +0530

    changes

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -25,7 +25,24 @@ const Profile = () => {
               <div className={profile.profile_container}>
                    
                    <div className={profile.profile}>
-                        
+                        <div className={profile.left}>
+
+                          <div className={profile.image}>
+                            <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" className={profile.avatar}/>
+                          </div>
+
+                        </div>
+                        <div className={profile.right}>
+                            <div className={profile.info}>
+                                
+                                <p> <span className={profile.credentials}>Student Name</span> Arnav Jain </p>
+                                <p> <span className={profile.credentials}>Student ID</span> 2110110900 </p>
+                                <p> <span className={profile.credentials}>Major</span> Bakchodi   </p>
+
+                            
+                                
+                            </div>
+                        </div>
                    </div>
               </div>
 
