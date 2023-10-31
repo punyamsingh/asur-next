3a9636b205de9f5fe140511cf89d3827851fe308
Rahuldj <rj712@snu.edu.in>
2023-10-31 23:25:52 +0530

    CheckLive-Endpoint-added

---
+++ src/pages/api/CheckLive.js
@@ -0,0 +1,30 @@
+// 
+
+import connection from './db.js';
+import middleware from '@/cors.js';
+
+export default async function(req, res) {
+  await middleware(req, res);
+  try {
+    if (req.method === 'GET') {
+        const{coursecode}=req.query;
+      const results = await new Promise((resolve, reject) => {
+        connection.query(`SELECT LIVE FROM subject where subject_id="${coursecode}"`, (error, results) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(results);
+          }
+        });
+      });
+      
+      res.status(200).json(results);
+    } else {
+      res.status(405).json({ error: 'Method not allowed' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching data' });
+  } finally {
+    res.end();
+  }
+};
