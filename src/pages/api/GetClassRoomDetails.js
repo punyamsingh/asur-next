5a69bfd6092f408a80ddcbe93fc31b079427dd58
Aayush Arora <89267748+aayush12arora@users.noreply.github.com>
2023-11-02 01:55:43 +0530

    Update GetClassRoomDetails.js

--- src/pages/api/GetClassRoomDetails.js
+++ src/pages/api/GetClassRoomDetails.js
@@ -1,24 +1,28 @@
-// 
-
 import connection from './db.js';
 
-export default async function(req, res) {
+export default async function (req, res) {
   try {
-   
     if (req.method === 'GET') {
-        const {coursecode} = req.query;
-        console.log(coursecode)
+      const { coursecode } = req.query;
+      console.log(coursecode);
       const results = await new Promise((resolve, reject) => {
-        connection.query(`SELECT * FROM classroom WHERE room_id = (SELECT classroom_id FROM subject WHERE Subject_ID = ${coursecode})`, (error, results) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(results);
+        connection.query(
+          `SELECT * FROM classroom WHERE room_id = (SELECT classroom_id FROM subject WHERE Subject_ID = ${coursecode})`,
+          (error, results) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(results);
+            }
           }
-        });
+        );
       });
-      
-      res.status(200).json(results);
+
+      if (results.length > 0) {
+        res.status(200).json(results[0]); // Return the first element as a single JSON object
+      } else {
+        res.status(404).json({ error: 'No matching data found' });
+      }
     } else {
       res.status(405).json({ error: 'Method not allowed' });
     }
@@ -27,4 +31,4 @@ export default async function(req, res) {
   } finally {
     res.end();
   }
-};
+}
