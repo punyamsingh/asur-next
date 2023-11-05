fa563f69226682738193162d5508f226101fdc3a
punyamsingh <89277920+punyamsingh@users.noreply.github.com>
2023-11-05 17:24:45 +0530

    Fix error handling in GetStudentViewWeb API

--- src/pages/api/GetStudentViewWeb.js
+++ src/pages/api/GetStudentViewWeb.js
@@ -19,16 +19,29 @@ export default async function (req,res) {
         JOIN attendance_details ON subject.subject_id = attendance_details.subject_id
         WHERE attendance_details.roll_no = ${rollNo}
         GROUP BY subject.subject_id, subject.subject_name, subject.teachername, subject.classroom_id;`,(error,results) => {
-          (error,results) => {
-            if (error) {
-              reject(error);
-            } else {
-              resolve(results);
-            }
-          };
+          if (error) {
+            reject(error);
+          } else {
+            resolve(results);
+          }
         });
       });
+
       res.status(200).json(results);
+      // if (results.length === 0) {
+      //   // Create the specific response structure with 0% for each subject
+      //   const emptyResponse = [
+      //     { "Subject_ID": "CCC708", "Percentage": 0 },
+      //     { "Subject_ID": "CSD101", "Percentage": 0 },
+      //     { "Subject_ID": "CSD102", "Percentage": 0 },
+      //     { "Subject_ID": "CSD311", "Percentage": 0 },
+      //     { "Subject_ID": "MAT376", "Percentage": 0 },
+      //     // Add more subjects as needed
+      //   ];
+      //   res.status(200).json(emptyResponse);
+      // } else {
+      //   res.status(200).json(results);
+      // }
     } else {
       res.status(405).json({ error: 'Method not allowed' });
     }
