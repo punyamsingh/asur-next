7c03683462870e4b5c075835835197364f1eb150
punyamsingh <89277920+punyamsingh@users.noreply.github.com>
2023-11-07 15:15:21 +0530

    Refactor code to improve readability and add functionality for initiating and ending classes

--- src/pages/teacher/dashboard.js
+++ src/pages/teacher/dashboard.js
@@ -6,109 +6,110 @@ import { HashLoader } from 'react-spinners';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 const Dashboard = () => {
-    const { data: apiData,error } = useSWR("/api/GetCourseList",fetcher);
-    const [liveCourses,setLiveCourses] = useState(new Set());
-    const [courseData,setCourseData] = useState([]);
-    const [loading,setLoading] = useState(true);
+  const { data: apiData,error } = useSWR("/api/GetCourseList",fetcher);
+  const [liveCourses,setLiveCourses] = useState(new Set());
+  const [courseData,setCourseData] = useState([]);
+  const [loading,setLoading] = useState(true);
 
-    const handleInitiateClick = async (course_id) => {
-        try {
-            const response = await fetch(
-                "https://asur-ams.vercel.app/api/toggleLive",
-                {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({ course_id }),
-                }
-            );
-
-            if (response.ok) {
-                if (liveCourses.has(course_id)) {
-                    liveCourses.delete(course_id);
-                } else {
-                    liveCourses.add(course_id);
-                }
-                setLiveCourses(new Set(liveCourses));
-            } else {
-                console.error("Error:",response.statusText);
-            }
-        } catch (error) {
-            console.error("Error:",error);
+  const handleInitiateClick = async (course_id) => {
+    try {
+      const response = await fetch(
+        "https://asur-ams.vercel.app/api/toggleLive",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ course_id }),
         }
-    };
+      );
 
-    useEffect(() => {
-        if (apiData) {
-            setCourseData(apiData);
-            // Set loading to false when data is successfully fetched
-            setLoading(false);
+      if (response.ok) {
+        if (liveCourses.has(course_id)) {
+          liveCourses.delete(course_id);
+        } else {
+          liveCourses.add(course_id);
         }
-    },[apiData]);
-
-    if (error) {
-        return (
-            <div>
-                <p>Error fetching data.</p>
-            </div>
-        );
+        setLiveCourses(new Set(liveCourses));
+      } else {
+        console.error("Error:",response.statusText);
+      }
+    } catch (error) {
+      console.error("Error:",error);
     }
+  };
 
+  useEffect(() => {
+    if (apiData) {
+      setCourseData(apiData);
+      // Set loading to false when data is successfully fetched
+      setLoading(false);
+    }
+  },[apiData]);
+
+  if (error) {
     return (
-        <div>
-            <div className={styles.navigation_area}>
-                <Navbar />
-            </div>
-
-            {loading ? (
-                <div className={styles.loader}>
-                    <HashLoader color="red" loading={true} size={50} />
-                </div>
-            ) : (
-                <div className={styles.container}>
-                    <table className={styles.table}>
-                        <thead>
-                            <tr>
-                                <th id={styles.sno} className={styles.th}>
-                                    S.No
-                                </th>
-                                <th id={styles.course_name} className={styles.th}>
-                                    Course Name
-                                </th>
-                                <th id={styles.attendance} className={styles.th}>
-                                    Initiate Attendance
-                                </th>
-                            </tr>
-                            <tr></tr>
-                            <tr></tr>
-                        </thead>
-
-                        <tbody>
-                            {courseData?.map((course,index) => (
-                                <tr key={index}>
-                                    <td id={styles.sno}>{index + 1}</td>
-                                    <td id={styles.subName}>
-                                        {course.Subject_Name} <br />
-                                    </td>
-                                    <td id={styles.button}>
-                                        <button
-                                            className={styles.button}
-                                            onClick={() => handleInitiateClick(course.Subject_ID)}
-                                        >
-                                            {liveCourses.has(course.Subject_ID)
-                                                ? "End Class"
-                                                : "Initiate"}
-                                        </button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            )}
-        </div>
+      <div>
+        <p>Error fetching data.</p>
+      </div>
     );
+  }
+
+  return (
+    <div>
+      <div className={styles.navigation_area}>
+        <Navbar />
+      </div>
+
+      {loading ? (
+        <div className={styles.loader}>
+          <HashLoader color="red" loading={true} size={50} />
+        </div>
+      ) : (
+        <div className={styles.container}>
+          <table className={styles.table}>
+            <thead>
+              <tr>
+                <th id={styles.sno} className={styles.th}>
+                  S.No
+                </th>
+                <th id={styles.course_name} className={styles.th}>
+                  Course Name
+                </th>
+                <th id={styles.attendance} className={styles.th}>
+                  Initiate Attendance
+                </th>
+              </tr>
+              <tr></tr>
+              <tr></tr>
+            </thead>
+
+            <tbody>
+              {courseData?.map((course,index) => (
+                <tr key={index}>
+                  <td id={styles.sno}>{index + 1}</td>
+                  <td id={styles.subName}>
+                    {course.Subject_Name} <br />
+                  </td>
+                  <td id={styles.button}>
+                    <button
+                      className={styles.button}
+                      onClick={() => handleInitiateClick(course.Subject_ID)}
+                    >
+                      {/* {liveCourses.has(course.Subject_ID)
+                        ? "End Class"
+                        : "Initiate"} */}
+                      {course.LIVE === "NL" ? "Initiate" : "End Class"}
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Dashboard;
