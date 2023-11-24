dac980d019dcbdef414edc4d5fe375a21fc7dbd0
Mayank Gupta <mayankgupta20231@gmail.com>
2023-11-24 22:42:47 +0530

    student profile css fix

--- src/pages/student/profile.js
+++ src/pages/student/profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { useUser } from '../../contexts/UserContext';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import useSWR from 'swr';
 import { useUser } from '@/contexts/UserContext';
 import Choice from '@/pages/Choice';
@@ -12,50 +12,48 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 // import styles from '@/styles/Profile.module.css';
 
 const Profile = () => {
-    const { userType,user} = useUser();
-    const [rollNumber,setRollNumber] = useState(null);
-    const[studProfile,setStudProfile] = useState(null);
+    const { userType, user } = useUser();
+    const [rollNumber, setRollNumber] = useState(null);
+    const [studProfile, setStudProfile] = useState(null);
 
-    const { data: rollNumberData,error: rollNumberError } = useSWR(
+    const { data: rollNumberData, error: rollNumberError } = useSWR(
         user ? `/api/GetRollNumFromEmail?email="${user.email}"` : null,
         fetcher
     );
 
-    const { data:studProfileData,error: studProfileError } = useSWR(
+    const { data: studProfileData, error: studProfileError } = useSWR(
         rollNumber ? `/api/GetStudentProfile?rollNo=${rollNumber}` : null,
         fetcher
     );
 
     useEffect(() => {
-        if (user)
-        {
-            
-        console.log(user.email)
+        if (user) {
+
+            console.log(user.email)
         }
         if (rollNumberData) {
             // console.log(rollNumberData[0]?.roll_no);
             setRollNumber(rollNumberData[0]?.roll_no);
             console.log(rollNumber)
-            if (rollNumber)
-            {
+            if (rollNumber) {
                 console.log(studProfileData)
             }
         }
-    },[rollNumberData],[studProfileData]);
+    }, [rollNumberData], [studProfileData]);
     // console.log(userType);
     return (
         <div>
             {userType ? ( // Check if userType is not null
                 <div>
-                    <div className={styles.navigation_bar}>
+                    <div className={profile.navigation_bar}>
                         <Navbar />
                     </div>
 
                     <div className={profile.profile_container}>
                         <div className={profile.profile}>
-                            
-                           
-                                {/* <div className={profile.info}>
+
+
+                            {/* <div className={profile.info}>
 
                                     <h4 className={styles.bi}>Basic Information</h4>
                                     
@@ -65,39 +63,39 @@ const Profile = () => {
 
                                 </div> */}
 
-                                {studProfileData ? (
-                                    <>
+                            {studProfileData ? (
+                                <>
                                     <div className={profile.left}>
-                                    <div className={profile.image}>
-                                        <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" className={profile.avatar} />
+                                        <div className={profile.image}>
+                                            <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" className={profile.avatar} />
+                                        </div>
+                                        <h1 className={profile.name}>{studProfileData[0]?.First_Name + " " + studProfileData[0].Last_Name}</h1>
                                     </div>
-                                    <h1 className={profile.name}>{studProfileData[0]?.First_Name+" "+studProfileData[0].Last_Name}</h1>
-                                </div>
 
                                     <div className={profile.right}>
-                                    <div className={profile.info}>
-                                        <h4 className={profile.bi}>Basic Information</h4>
-                                        <p className={profile.white}>
-                                        <span className={profile.credentials}>Student ID</span>
-                                        {studProfileData[0]?.Roll_No}
-                                        </p>
-                                        <p className={profile.white}>
-                                        <span className={profile.credentials}>Major</span>
-                                        CSE
-                                        </p>
-                                        <p className={profile.white}>
-                                        <span className={profile.credentials}>E-Mail</span>
-                                        {user?.email}
-                                        </p>
+                                        <div className={profile.info}>
+                                            <h4 className={profile.bi}>Basic Information</h4>
+                                            <p className={profile.white}>
+                                                <span className={profile.credentials}>Student ID</span>
+                                                {studProfileData[0]?.Roll_No}
+                                            </p>
+                                            <p className={profile.white}>
+                                                <span className={profile.credentials}>Major</span>
+                                                CSE
+                                            </p>
+                                            <p className={profile.white}>
+                                                <span className={profile.credentials}>E-Mail</span>
+                                                {user?.email}
+                                            </p>
+                                        </div>
                                     </div>
-                                    </div>
-                                    </>
-                                    ) : (
-                                        <div className={styles.loader}>
-                                        <HashLoader color="red" loading={true} size={50} />
-                                    </div>
-                                    )}
-                            
+                                </>
+                            ) : (
+                                <div className={styles.loader}>
+                                    <HashLoader color="red" loading={true} size={50} />
+                                </div>
+                            )}
+
                         </div>
                     </div>
                 </div>
