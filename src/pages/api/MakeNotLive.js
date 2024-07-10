145b9100db680127c3c132a1705afb251b92d0e8
punyamsingh <89277920+punyamsingh@users.noreply.github.com>
2024-07-10 20:53:08 +0530

    Refactor MakeNotLive API endpoint to use Supabase

--- src/pages/api/MakeNotLive.js
+++ src/pages/api/MakeNotLive.js
@@ -1,28 +1,21 @@
-import connection from './db.js';
+import supabase from './db.js';
 import middleware from '@/cors.js';
 
-export default async function handler(req, res) {
+export default async function handler(req,res) {
   if (req.method === 'POST') {
-    await middleware(req, res);
+    await middleware(req,res);
     try {
-        const {course_id}=req.body
+      const { course_id } = req.body;
 
-      // Your SQL query
-      const query = `
-      UPDATE subject
-  SET  LIVE= "NL"
-  WHERE subject_id="${course_id}"
-      `;
+      // Update the subject to set LIVE to 'NL'
+      const { data,error } = await supabase
+        .from('subject')
+        .update({ LIVE: 'NL' })
+        .eq('Subject_ID',course_id);
 
-      const results = await new Promise((resolve, reject) => {
-        connection.query(query, [course_id], (error, results) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(results);
-          }
-        });
-      });
+      if (error) {
+        throw error;
+      }
 
       res.status(200).json({ message: 'Class not live successful' });
     } catch (error) {
