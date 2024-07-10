1b0e7ff2cf0c89c8c02643cffe1cd66d140e71ed
punyamsingh <89277920+punyamsingh@users.noreply.github.com>
2024-07-10 20:35:04 +0530

    chore: Refactor GetCourseWiseWebAtt API endpoint to use Supabase

--- src/pages/api/GetCourseWiseWebAtt.js
+++ src/pages/api/GetCourseWiseWebAtt.js
@@ -1,28 +1,26 @@
-// 
+import supabase from './db.js';
 
-import connection from './db.js';
-
-export default async function(req, res) {
+export default async function handler(req,res) {
   try {
-   
     if (req.method === 'GET') {
-        const { rollNo,courseId} = req.query;
-      const results = await new Promise((resolve, reject) => {
-        connection.query(`select * from attendance_details where roll_no=${rollNo} and subject_id=${courseId}`, (error, results) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(results);
-          }
-        });
-      });
-      
-      res.status(200).json(results);
+      const { rollNo,courseId } = req.query;
+
+      const { data,error } = await supabase
+        .from('attendance_details')
+        .select('*')
+        .eq('Roll_No',rollNo)
+        .eq('Subject_ID',courseId);
+
+      if (error) {
+        throw error;
+      }
+
+      res.status(200).json(data);
     } else {
       res.status(405).json({ error: 'Method not allowed' });
     }
   } catch (error) {
-    res.status(500).json({ error: 'Error fetching data' });
+    res.status(500).json({ error: 'Error fetching data',details: error.message });
   } finally {
     res.end();
   }
