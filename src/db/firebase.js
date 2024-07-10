3261ce49845378d5aebd7d700962e6006df85862
punyamsingh <89277920+punyamsingh@users.noreply.github.com>
2024-07-10 21:18:02 +0530

    chore: Refactor Firebase configuration to use environment variables

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -1,22 +1,14 @@
 // firebase.js
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyC781HJMfrgCHl8tAc1KOZEYGLn2LLbMIs",
-    authDomain: "asur-snu.firebaseapp.com",
-    projectId: "asur-snu",
-    storageBucket: "asur-snu.appspot.com",
-    messagingSenderId: "653677327927",
-    appId: "1:653677327927:web:eaa4a9377225e3980db1b2",
-    measurementId: "G-84E4XWJPJX"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 export const app = initializeApp(firebaseConfig);
-// if (!firebase.apps.length) {
-//     firebase.initializeApp({});
-//  }else {
-//     firebase.app(); // if already initialized, use that one
-//  }
-// // const analytics = getAnalytics(app);
-
-// export default app;
--- src/pages/api/db.js
+++ src/pages/api/db.js
@@ -1,30 +1,4 @@
 // db.js
-// import mysql from 'mysql';
-
-// const connection = mysql.createConnection({
-//   host:"sql12.freesqldatabase.com",
-//   user: "sql12719142",
-//   password: "zv2Vuh5uLG",
-//   database: "sql12719142",
-// });
-
-// const connection = mysql.createConnection({
-//   host: process.env.NEXT_PUBLIC_DB_HOST,
-//   user: process.env.NEXT_PUBLIC_DB_USER,
-//   password: process.env.NEXT_PUBLIC_DB_PASSWORD,
-//   database: process.env.NEXT_PUBLIC_DB_DATABASE,
-// });
-
-// connection.connect((err) => {
-//   if (err) {
-//     console.error('Database connection error: ' + err.stack);
-//     return;
-//   }
-//   // console.log('Connected to the database');
-// });
-
-// export default connection;
-
 
 import { createClient } from '@supabase/supabase-js';
 
