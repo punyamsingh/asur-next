00950909e7c257d7b39a8d97247d991def6c1abf
punyamsingh <89277920+punyamsingh@users.noreply.github.com>
2024-07-10 20:50:47 +0530

    chore: added code for our first ever rpc.

---
+++ src/db/get_attendance_details_rpc.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FUNCTION public.get_attendance_details(roll_no integer)
+RETURNS TABLE (
+    subject_id text,
+    subject_name text,
+    teachername text,
+    classroom_id text,
+    P_count integer,
+    A_count integer,
+    attendance_percent double precision
+) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        subject.subject_id,
+        subject.subject_name,
+        subject.teachername,
+        subject.classroom_id,
+        COUNT(CASE WHEN attendance_details.PorA = 'P' THEN 1 ELSE NULL END) AS P_count,
+        COUNT(CASE WHEN attendance_details.PorA = 'A' THEN 1 ELSE NULL END) AS A_count,
+        (COUNT(CASE WHEN attendance_details.PorA = 'P' THEN 1 ELSE NULL END) / COUNT(*)) * 100 AS attendance_percent
+    FROM subject
+    JOIN attendance_details ON subject.subject_id = attendance_details.subject_id
+    WHERE attendance_details.roll_no = roll_no
+    GROUP BY subject.subject_id, subject.subject_name, subject.teachername, subject.classroom_id;
+END;
+$$ LANGUAGE plpgsql;
