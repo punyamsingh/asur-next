6ccd48edc1f93d7f10e1ccb00888b6b854ae6e5c
punyamsingh <89277920+punyamsingh@users.noreply.github.com>
2024-10-15 21:13:15 +0530

    Refactor teacher dashboard component to use useSWR for fetching course data

--- src/pages/teacher/dashboard.js
+++ src/pages/teacher/dashboard.js
@@ -1,21 +1,30 @@
+import useSWR from "swr";
 import Navbar from "./teacherNavbar";
 import styles from "@/styles/Teacher.module.css";
-import { useState } from "react";
-import { HashLoader,BeatLoader } from 'react-spinners';
+import { useState,useEffect } from "react";
+import { HashLoader,BeatLoader } from "react-spinners";
 
-const Dashboard = ({ initialCourseData }) => {
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+const Dashboard = () => {
+  const { data: apiData,error } = useSWR("/api/GetCourseList",fetcher);
   const [liveCourses,setLiveCourses] = useState(new Set());
-  const [courseData,setCourseData] = useState(initialCourseData);
+  const [courseData,setCourseData] = useState([]);
+  const [loading,setLoading] = useState(true);
 
   const handleInitiateClick = async (course_id) => {
     try {
-      const courseToUpdate = courseData.find((course) => course.subject_id === course_id);
-      if (courseToUpdate) {
-        courseToUpdate.loading = true;
-        setCourseData([...courseData]);
+      const courseIndex = courseData.findIndex(
+        (course) => course.subject_id === course_id
+      );
+
+      if (courseIndex >= 0) {
+        const updatedCourseData = [...courseData];
+        updatedCourseData[courseIndex].loading = true;
+        setCourseData(updatedCourseData);
       }
 
-      const response = await fetch("/api/toggleLive",{
+      const response = await fetch("https://asur-ams.vercel.app/api/toggleLive",{
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -24,14 +33,13 @@ const Dashboard = ({ initialCourseData }) => {
       });
 
       if (response.ok) {
-        if (liveCourses.has(course_id)) {
-          liveCourses.delete(course_id);
+        const updatedLiveCourses = new Set(liveCourses);
+        if (updatedLiveCourses.has(course_id)) {
+          updatedLiveCourses.delete(course_id);
         } else {
-          liveCourses.add(course_id);
+          updatedLiveCourses.add(course_id);
         }
-        setLiveCourses(new Set(liveCourses));
 
-        // Update the live status and loading state of the course
         const updatedCourseData = courseData.map((course) => {
           if (course.subject_id === course_id) {
             return {
@@ -42,6 +50,8 @@ const Dashboard = ({ initialCourseData }) => {
           }
           return course;
         });
+
+        setLiveCourses(updatedLiveCourses);
         setCourseData(updatedCourseData);
       } else {
         console.error("Error:",response.statusText);
@@ -51,70 +61,76 @@ const Dashboard = ({ initialCourseData }) => {
     }
   };
 
+  useEffect(() => {
+    if (apiData && apiData.length > 0) {
+      const updatedCourseData = apiData.map((course) => ({
+        ...course,
+        liveStatus: course.live === "NL" ? "Initiate" : "End Class",
+        loading: false, // Initialize loading state per course
+      }));
+      setCourseData(updatedCourseData);
+      setLoading(false);
+    }
+  },[apiData]);
+
+  if (error) {
+    return (
+      <div>
+        <p>Error fetching data.</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <div className={styles.navigation_area}>
         <Navbar />
       </div>
 
-      <div className={styles.container}>
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <th id={styles.sno} className={styles.th}>
-                Course ID
-              </th>
-              <th id={styles.course_name} className={styles.th}>
-                Course Name
-              </th>
-              <th id={styles.attendance} className={styles.th}>
-                Initiate Attendance
-              </th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {courseData?.length > 0 && courseData.map((course,index) => (
-              <tr key={index}>
-                <td id={styles.sno}>{course.subject_id}</td>
-                <td id={styles.subName}>{course.subject_name}</td>
-                <td id={styles.button}>
-                  <button
-                    className={`${course.liveStatus === "Initiate" ? styles.nonActiveButton : styles.activeButton}`}
-                    onClick={() => handleInitiateClick(course.subject_id)}
-                  >
-                    {course.loading ? (
-                      <BeatLoader color="red" loading={true} />
-                    ) : (
-                      course.liveStatus
-                    )}
-                  </button>
-                </td>
+      {loading ? (
+        <div className={styles.loader}>
+          <HashLoader color="red" loading={true} size={50} />
+        </div>
+      ) : (
+        <div className={styles.container}>
+          <table className={styles.table}>
+            <thead>
+              <tr>
+                <th id={styles.sno} className={styles.th}>Course ID</th>
+                <th id={styles.course_name} className={styles.th}>Course Name</th>
+                <th id={styles.attendance} className={styles.th}>Initiate Attendance</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {courseData.length > 0 &&
+                courseData.map((course,index) => (
+                  <tr key={index}>
+                    <td id={styles.sno}>{course.subject_id}</td>
+                    <td id={styles.subName}>{course.subject_name}</td>
+                    <td id={styles.button}>
+                      <button
+                        className={
+                          course.liveStatus === "Initiate"
+                            ? styles.nonActiveButton
+                            : styles.activeButton
+                        }
+                        onClick={() => handleInitiateClick(course.subject_id)}
+                      >
+                        {course.loading ? (
+                          <BeatLoader color="red" loading={true} />
+                        ) : (
+                          course.liveStatus
+                        )}
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
-
-// Fetching data from the API on the server side
-export async function getServerSideProps() {
-  const response = await fetch("https://asur-ams.vercel.app/api/GetCourseList");
-  const initialCourseData = await response.json();
-
-  // Modify the response to add liveStatus to each course
-  const modifiedCourseData = initialCourseData.map((course) => ({
-    ...course,
-    liveStatus: course.live === "NL" ? "Initiate" : "End Class",
-  }));
-
-  return {
-    props: {
-      initialCourseData: modifiedCourseData,
-    },
-  };
-}
 
 export default Dashboard;
